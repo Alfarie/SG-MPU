2ed50d8919b64677c19d4b3a2c7dd53c014c938a
Farhan Abuhazan <farhan.abuhazan@gmail.com>
2018-03-07 14:28:58 +0700

    fix sensor module

--- src/modules/models/sensors.js
+++ src/modules/models/sensors.js
@@ -2,8 +2,8 @@ module.exports = {
     sensor: {
         par: 53,
         vpd: 15032,
-        temperature: 23,
-        humidity:54,
+        temp: 23,
+        humi:54,
         soil: 62,
         date: '2017-01-01',
         time: '00:00:00'
