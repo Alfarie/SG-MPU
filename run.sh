c751efe512fb6b81441d9617499cc20ef0422684
Alfarie <farhan.abuhazan@gmail.com>
2018-08-07 10:52:47 +0700

    rpi  run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ echo "Script location: ${BASEDIR}"
 
 
 KILLP="sudo pkill -9 node"
-CMD="sudo node ${BASEDIR}/src/main.js -sp=scan -prod  --iface=wlan0 --loggerTime=300000"
+CMD="sudo node ${BASEDIR}/src/main.js -sp=/dev/serial0 --iface=wlan0 --loggerTime=1000 -prod --wificustom"
 
 echo "[INFO] KILLING NodeJs Process..."
 $KILLP
